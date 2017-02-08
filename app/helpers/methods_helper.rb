a4cf9e4963fcc98ee9a2d1896dedca7e12553d3a
WAFulbright <bill@qa2100.com>
2017-02-08 14:42:09 -0500

    Many changes to project name from my_clean, myclean to tdmportal, all new templates for presentation and appropriate code to embed videos

--- app/helpers/methods_helper.rb
+++ app/helpers/methods_helper.rb
@@ -1,2 +1,7 @@
 module MethodsHelper
-end
+  def embed(youtube_url)
+    youtube_id = youtube_url.split("/").last
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", width: "360", height: "215")
+  end
+
+end  
--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,2 +1,8 @@
 module ServicesHelper
-end
+
+  def embed(youtube_url)
+    youtube_id = youtube_url.split("/").last
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", width: "360", height: "215")
+  end
+
+end  
